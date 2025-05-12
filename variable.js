0e2852da2464ace32a629f6e2b00777547cb49b3
ankursinha19 <ankursinha197@gmail.com>
2025-05-12 22:09:43 +0530

    var let const

---
+++ variable.js
@@ -0,0 +1,34 @@
+// let typeLet = "Let variable"
+// const typeConst = "const variable"
+// var typevar = "var variable"
+
+// function varScope() {
+//     console.log(typeLet)
+//     console.log(typeConst)
+//     console.log(typevar)
+// }
+// varScope()
+// var typevar = "vardsfdf variable"
+// // let typeLet = "Let variable"
+// // typeConst = "const ddfsdvariable"
+
+//------------Example 2------------------------------------//
+
+function varScope() {
+
+    let typeLet = "Let variable"
+    const typeConst = "const variable"
+    var typevar = "var variable"
+
+
+    console.log(typeLet)
+    console.log(typeConst)
+    console.log(typevar)
+}
+varScope()
+
+
+
+// console.log(typeLet)
+// console.log(typeConst)
+// console.log(typevar)
